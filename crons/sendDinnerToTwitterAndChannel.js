aa9a745d5ae692fbccd019c28b2b74ce12c043a1
Enes Çakır <enes@cakir.web.tr>
2017-05-11 00:20:22 +0300

    Dinner cron comment out

--- crons/sendDinnerToTwitterAndChannel.js
+++ crons/sendDinnerToTwitterAndChannel.js
@@ -1,6 +1,6 @@
 var bot = require('../helpers/yemekciBot');
+var dotenv = require('dotenv').config();
 
-
-//var today = new Date();
-//bot.sendDinner(today);
-//bot.sendDinnerToTwitter(today);
+var today = new Date();
+bot.sendDinner(today);
+bot.sendDinnerToTwitter(today);
