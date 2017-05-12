3157aac3f80d28e86eeba68b6abda9b58ef71fc6
Enes Çakır <enes@cakir.web.tr>
2017-05-12 14:13:27 +0300

    Message creation redundancy refactored

--- helpers/yemekciBot.js
+++ helpers/yemekciBot.js
@@ -3,17 +3,15 @@ var processor = require('../helpers/processor');
 var constants = require('../constants/fileConstants');
 var redis = require('redis');
 var dotenv = require('dotenv').config()
+var Twitter = require('twit');
 var moment = require('moment');
 require('moment/locale/tr');
-var Twitter = require('twit');
 
 var bot = new SlackBot({
     token: process.env.BOT_TOKEN,
     name: process.env.BOT_NAME,
 });
-
 var channel = process.env.BOT_CHANNEL;
-
 var twitter = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -31,11 +29,10 @@ function getYemek(date, onSuccess, onError) {
     if(!yemekJson) {
       onError();
       console.log(err);
-    }else{
+    } else {
       yemekObject = JSON.parse(yemekJson);
       onSuccess(yemekObject);
     }
-
     client.quit();
   });
 }
@@ -52,79 +49,74 @@ function getDinner(date, onSuccess, onError) {
   }, onError);
 }
 
+function createMessage(date, meals, time, isMarkdown, onSuccess, onError) {
+  var bold = "";
+  var quote = "";
+
+  if(isMarkdown){
+    bold = "*";
+    quote = "> ";
+  }
+  var message = bold + moment(date).format("D MMMM dddd") + " " + time + " yemeği:" + bold + "\n";
+  for (var i = 0; i < meals.length; i++)
+    message += quote + meals[i] + "\n";
+  message += bold + "Afiyet olsun!" + bold + " :stew:";
+  onSuccess(message);
+}
+
 function sendLunchToChannel(date, channel, onSuccess, onError) {
-  getLunch(date, function onSuccess(data) {
-    var meals = data;
-    var message = "*" + moment(date).format("D MMMM dddd") + " öğle yemeği:*\n"
-    for (var i = 0; i < meals.length; i++) {
-      message += "> " + meals[i] + "\n"
-    }
-    message += "*Afiyet olsun!* :stew:"
-    bot.postMessage(channel, message);
+  getLunch(date, function onSuccess(meals) {
+    createMessage(date, meals, "öğle", true, function onSuccess(message) {
+      bot.postMessage(channel, message);
+    })
   });
 }
 
 function sendLunchToTwitter(date, onSuccess, onError) {
   getLunch(date, function onSuccess(meals) {
-    var message = moment(date).format("D MMMM dddd") + " öğle yemeği:\n"
-    for (var i = 0; i < meals.length; i++) {
-      message += meals[i] + "\n";
-    }
-    message += "Afiyet olsun!"
-
-    twitter.post('statuses/update', { status: message }, function(err, data, response) {
-      console.log(err);
+    createMessage(date, data, "öğle", false, function onSuccess(message) {
+      twitter.post('statuses/update', { status: message }, function(err, data, response) {
+        if(err){
+          console.log(err);
+        }
+      })
     })
   });
 }
 
 function sendDinnerToChannel(date, channel, successCallback, errorCallback) {
-  getDinner(date, function onSuccess(data) {
-    var meals = data;
-    var message = "*" + moment(date).format("D MMMM dddd") + " akşam yemeği:*\n"
-    for (var i = 0; i < meals.length; i++) {
-      message += "> " + meals[i] + "\n"
-    }
-    message += "*Afiyet olsun!* :stew:"
-    bot.postMessage(channel, message);
+  getDinner(date, function onSuccess(meals) {
+    createMessage(date, meals, "akşam", true, function onSuccess(message) {
+      bot.postMessage(channel, message);
+    })
   });
 }
 
 function sendDinnerToTwitter(date, onSuccess, onError) {
   getDinner(date, function onSuccess(meals) {
-    var message = moment(date).format("D MMMM dddd") + " akşam yemeği:\n"
-    for (var i = 0; i < meals.length; i++) {
-      message += meals[i] + "\n";
-    }
-    message += "Afiyet olsun!"
-
-    twitter.post('statuses/update', { status: message }, function(err, data, response) {
-      console.log(err);
+    createMessage(date, data, "akşam", false, function onSuccess(message) {
+      twitter.post('statuses/update', { status: message }, function(err, data, response) {
+        if(err){
+          console.log(err);
+        }
+      })
     })
   });
 }
 
 function sendLunchToUser(date, userChannel, onSuccess, onError) {
-  getLunch(date, function onSuccess(data) {
-    var meals = data;
-    var message = "*" + moment(date).format("D MMMM dddd") + " öğle yemeği:*\n"
-    for (var i = 0; i < meals.length; i++) {
-      message += "> " + meals[i] + "\n"
-    }
-    message += "*Afiyet olsun!* :stew:"
-    bot.postMessage(userChannel, message);
+  getLunch(date, function onSuccess(meals) {
+    createMessage(date, meals, "öğle", true, function onSuccess(message) {
+      bot.postMessage(userChannel, message);
+    })
   });
 }
 
 function sendDinnerToUser(date, userChannel, onSuccess, onError) {
-  getDinner(date, function onSuccess(data) {
-    var meals = data;
-    var message = "*" + moment(date).format("D MMMM dddd") + " akşam yemeği:*\n"
-    for (var i = 0; i < meals.length; i++) {
-      message += "> " + meals[i] + "\n"
-    }
-    message += "*Afiyet olsun!* :stew:"
-    bot.postMessage(userChannel, message);
+  getDinner(date, function onSuccess(meals) {
+    createMessage(date, meals, "akşam", true, function onSuccess(message) {
+      bot.postMessage(userChannel, message);
+    })
   });
 }
 
