78b07ed084ba1454eca0af4997bc737edd33d6b6
ergun1017 <erdogmusergun@gmail.com>
2017-05-13 20:17:55 +0300

    Welcome and Left messages added

---
+++ constants/messageConstants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  CHANNEL_WELCOME_MSG = "#yemek kanalına hoş geldin",
+  FIRST_MSG = "Selam! Bana _`öğle, akşam, bugün, yarın, dün`_ gibi anahtar kelimelerle soru sorarsan sana yemeği söylerim.",
+  LEFT_MSG =  "Ah, hoşçakal! Okulun yemekleriyle işin bitti demek :smile:"
+}
