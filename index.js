a2b98f53605df1cfe68ee2eaa07f7318c12e0676
ergun1017 <erdogmusergun@gmail.com>
2017-10-17 13:12:37 +0300

    middleware enabling cors

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ var importer = new Importer({
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get('/:day/:month/:year', function(req, res) {
   var dd = req.params.day;
   var mm = req.params.month;
